60775e80a107327df49ab0287ddb9cd291e05771
ＴＲ <public@technotter.com>
2020-03-09 22:51:22 +0900

    Update TR.BIDS.TimeOnLCD.ino
    AutoSendなしバージョンを追加

--- TR.BIDS.TimeOnLCD/TR.BIDS.TimeOnLCD.ino
+++ TR.BIDS.TimeOnLCD/TR.BIDS.TimeOnLCD.ino
@@ -1,6 +1,8 @@
 //TR.BIDS.TimeOnLCD.ino
 //Under the MIT License
 //Copyright 2020 Tetsu Otter
+//#define AUTOSEND
+
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
@@ -17,10 +19,12 @@ void setup()
   Serial.begin(19200);
   while (!Serial);
   Serial.setTimeout(1);
+#ifdef AUTOSEND
   ASSet(10);//Hour
   ASSet(11);//Min
   ASSet(12);//Sec
-  lcd.setCursor(0,0);
+#endif
+  lcd.setCursor(0, 0);
   lcd.print("init complete.");
   delay(500);
   lcd.clear();
@@ -31,50 +35,62 @@ int MinRec = -1;
 int SecRec = -1;
 void loop()
 {
+  int h = HourRec;
+  int m = MinRec;
+  int sec = SecRec;
+
+#ifdef AUTOSEND
   while (Serial.available() <= 0);
   char c[32];
   ZeroFill(c, 32);
   if (Serial.readBytesUntil('\n', c, 32) < 4) ErrorLED(0x03);
 
   String s = String(c);
-  char ca[5];
+
+
   if (s.startsWith("TRIE10X")) {
     s.remove(0, 7);
-    int h = s.toInt();
-    if (HourRec != h) {
-      lcd.setCursor(0, 0);
-      lcd.print((h / 10) % 10);
-      lcd.setCursor(1, 0);
-      lcd.print(h % 10);
-    }
-    return;
+    h = s.toInt();
   }
   if (s.startsWith("TRIE11X")) {
     s.remove(0, 7);
-    int h = s.toInt();
-    if (MinRec != h) {
-      lcd.setCursor(3, 0);
-      lcd.print((h / 10) % 10);
-      lcd.setCursor(4, 0);
-      lcd.print(h % 10);
-    }
-    return;
+    m = s.toInt();
   }
   if (s.startsWith("TRIE12X")) {
     s.remove(0, 7);
-    int h = s.toInt();
-    if (SecRec != h) {
-      lcd.setCursor(6, 0);
-      lcd.print((h / 10) % 10);
-      lcd.setCursor(7, 0);
-      lcd.print(h % 10);
-    }
-    return;
+    sec = s.toInt();
   }
+
   lcd.setCursor(0, 1);
   lcd.print(">               ");
   lcd.setCursor(1, 1);
   lcd.print(s);
+
+#else
+  if (!BIDSReqInt("TRIE10\n", &h))ErrorLED(10);//Hour
+  if (!BIDSReqInt("TRIE11\n", &m))ErrorLED(11);//Min
+  if (!BIDSReqInt("TRIE12\n", &sec))ErrorLED(12);//Sec
+#endif
+
+  if (HourRec != h) {
+    lcd.setCursor(0, 0);
+    lcd.print((h / 10) % 10);
+    lcd.setCursor(1, 0);
+    lcd.print(h % 10);
+  }
+  if (MinRec != m) {
+    lcd.setCursor(3, 0);
+    lcd.print((m / 10) % 10);
+    lcd.setCursor(4, 0);
+    lcd.print(m % 10);
+  }
+  if (SecRec != sec) {
+    lcd.setCursor(6, 0);
+    lcd.print((sec / 10) % 10);
+    lcd.setCursor(7, 0);
+    lcd.print(sec % 10);
+  }
+
 }
 
 void ASSet(int num) {
@@ -127,4 +143,25 @@ void ErrorLED(byte ec) {
 void ZeroFill(char *c, int len) {
   for (int i = 0; i < len; i++)
     c[i] = 0;
+}
+
+
+bool BIDSReqInt(char* cmd, int * val) {
+  int i = 0;
+  Serial.print(cmd);
+  while (Serial.available() <= 0);
+  char c[64];
+  ZeroFill(c, 64);
+  String s;
+  if (Serial.readBytesUntil('\n', c, 64) < 4) ErrorLED(0x03);
+  
+  for (i = 0; i < 4; i++)
+    if (cmd[i] != c[i])return false;
+  i = 0;
+  while (c[i] != 'X' && c[i] != '\n' && cmd[i] != '\0') i++;
+  
+  s = String(c);
+  s.remove(0, i + 1);
+  *val = s.toInt();
+  return true;
 }
